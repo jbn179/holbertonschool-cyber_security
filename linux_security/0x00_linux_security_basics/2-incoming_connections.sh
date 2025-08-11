468ce11f8226245c83a412ac7faf2bc29c8d5010
jbn179 <9772@holbertonstudents.com>
2025-08-11 11:02:17 +0200

    Refactor formatting in 2-incoming_connections.sh for improved readability

--- linux_security/0x00_linux_security_basics/2-incoming_connections.sh
+++ linux_security/0x00_linux_security_basics/2-incoming_connections.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-iptables -F # Flush all rules
-iptables -P INPUT DROP # Drop all incoming traffic
-iptables -P FORWARD DROP # Drop all forwarded traffic
-iptables -P OUTPUT ACCEPT # Allow all outgoing traffic
-iptables -A INPUT -p tcp --dport 80 -j ACCEPT # Allow HTTP traffic on port 80
-
+iptables -F  # Flush all rules
+iptables -P INPUT DROP  # Drop all incoming traffic
+iptables -P FORWARD DROP  # Drop all forwarded traffic
+iptables -P OUTPUT ACCEPT  # Allow all outgoing traffic
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT  # Allow HTTP traffic on port 80
