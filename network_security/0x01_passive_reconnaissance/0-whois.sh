f59c4b625e93b4ee076ce9dac64e546d62fb7d6a
jbn179 <9772@holbertonstudents.com>
2025-08-13 18:47:55 +0200

    Refactor output generation in 0-whois.sh to simplify CSV formatting by replacing printf with a more concise awk command, enhancing the extraction of relevant fields (Registrant, Admin, Tech) for passive reconnaissance.

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk -F': ' '/^(Registrant|Admin|Tech)/ {printf "%s,%s\n", $1, $2}' > "$1.csv"
+whois $1 | awk '/^(Registrant|Admin|Tech).*:/ {sub(/: /, ","); print}' > $1.csv
