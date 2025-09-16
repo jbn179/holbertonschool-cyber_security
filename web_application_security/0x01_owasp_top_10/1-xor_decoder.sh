062d2ac7d20a91b394d4dfc21d42ef59b1e6b751
jbn179 <9772@holbertonstudents.com>
2025-09-16 12:32:32 +0200

    Simplify XOR decoder script by replacing manual decoding with a single Python command

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,18 +1,2 @@
 #!/bin/bash
-
-input="$1"
-input="${input#'{xor}'}"
-decoded_input=$(echo "$input" | base64 -d)
-output=""
-
-i=0
-while [ $i -lt ${#decoded_input} ]
-do
-	char="${decoded_input:$i:1}"
-	ascii_val=$(printf "%d" "'$char")
-	xor_result=$(( ascii_val ^ 95 ))
-	output+=$(printf "\\$(printf '%03o' $xor_result)")
-	i=$((i + 1))
-done
-
-echo "$output"
+python3 -c "from base64 import b64decode; print(bytes(byte ^ 0x5f for byte in b64decode('$1'.replace('{xor}', ''))).decode('utf-8'))"
