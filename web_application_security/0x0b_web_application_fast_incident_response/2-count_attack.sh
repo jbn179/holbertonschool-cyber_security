8a526e3975238abc0c9043cf188f325db9240d0f
jbn179 <9772@holbertonstudents.com>
2025-11-27 10:25:23 +0100

    Refactor count_attack.sh to directly extract the most frequent log entry count

--- web_application_security/0x0b_web_application_fast_incident_response/2-count_attack.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/2-count_attack.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
-grep -c "^$(awk '{print $1}' logs.txt | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}')" logs.txt
+#/bin/bash
+awk '{print $1}' logs.txt | sort | uniq -c | sort -nr | head -n 1 | awk '{print $1}'
