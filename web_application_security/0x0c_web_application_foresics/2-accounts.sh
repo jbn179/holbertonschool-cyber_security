4237605c5ac2ba91511dd01cc0d15096be14f658
jbn179 <9772@holbertonstudents.com>
2025-12-02 05:33:26 +0100

    Refactor 2-accounts.sh to simplify login attempt filtering logic

--- web_application_security/0x0c_web_application_foresics/2-accounts.sh
+++ web_application_security/0x0c_web_application_foresics/2-accounts.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-comm -12 <(tail -1000 auth.log | grep "Accepted password" | grep -v "invalid user" | awk '{print $9}' | sort -u) <(tail -1000 auth.log | grep "Failed password" | grep -oP 'for \K\w+' | sort -u)
+comm -12 <(tail -1000 auth.log | grep "Accepted password" | awk '{print $9}' | sort -u) <(tail -1000 auth.log | grep "Failed password" | awk '{print $9}' | sort -u)
