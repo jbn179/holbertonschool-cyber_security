221debaf3174c3fe70c361289ed62ab0f88ecc84
jbn179 <9772@holbertonstudents.com>
2026-01-13 19:29:05 +0100

    Add get_request function to fetch and display JSON data from a URL

---
+++ scripting_cyber/0x00-ruby_scripting/6-get.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+require 'net/http'
+require 'uri'
+require 'json'
+
+def get_request(url)
+  uri = URI(url)
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = (uri.scheme == 'https')
+
+  response = http.get(uri.request_uri)
+
+  puts "Response status: #{response.code} #{response.message}"
+  puts "\nResponse body:"
+
+  begin
+    json_data = JSON.parse(response.body)
+    puts JSON.pretty_generate(json_data)
+  rescue JSON::ParserError
+    puts response.body
+  end
+end
