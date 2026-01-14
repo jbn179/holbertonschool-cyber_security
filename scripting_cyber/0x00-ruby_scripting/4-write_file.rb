0e69cae7fbb183db6e1f6491ff65f68b5a597b6b
jbn179 <9772@holbertonstudents.com>
2026-01-14 13:29:46 +0100

    Refactor file writing method to use block for better resource management

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -8,5 +8,7 @@ def merge_json_files(file1_path, file2_path)
 
   merged_data = data2 + data1
 
-  File.write(file2_path, JSON.pretty_generate(merged_data))
+  File.open(file2_path, 'w') do |f|
+    f.write(JSON.pretty_generate(merged_data))
+  end
 end
