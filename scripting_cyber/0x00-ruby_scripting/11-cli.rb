81f1d1a02565e5779b16a3bd9bb7d7316e88678e
jbn179 <9772@holbertonstudents.com>
2026-01-14 12:37:33 +0100

    Refactor CLI options for improved clarity and update tasks file with a new task

--- scripting_cyber/0x00-ruby_scripting/11-cli.rb
+++ scripting_cyber/0x00-ruby_scripting/11-cli.rb
@@ -8,22 +8,18 @@ options = {}
 
 OptionParser.new do |opts|
   opts.banner = "Usage: cli.rb [options]"
-  opts.separator ""
 
   opts.on("-a", "--add TASK", "Add a new task") do |task|
     options[:add] = task
   end
-  opts.separator ""
 
   opts.on("-l", "--list", "List all tasks") do
     options[:list] = true
   end
-  opts.separator ""
 
   opts.on("-r", "--remove INDEX", Integer, "Remove a task by index") do |index|
     options[:remove] = index
   end
-  opts.separator ""
 
   opts.on("-h", "--help", "Show help") do
     puts opts
@@ -39,7 +35,7 @@ if options[:add]
 elsif options[:list]
   if File.exist?(TASKS_FILE)
     File.foreach(TASKS_FILE) do |line|
-      puts line
+      print line
     end
   end
 elsif options[:remove]
